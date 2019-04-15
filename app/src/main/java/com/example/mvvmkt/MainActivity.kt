5789a373a7d4648746d2a8ec3fa4253b87156c88
DanielLopez <d.lopez.torres@accenture.com>
2019-04-15 15:23:20 +0200

    [WIP]

--- app/src/main/java/com/example/mvvmkt/MainActivity.kt
+++ app/src/main/java/com/example/mvvmkt/MainActivity.kt
@@ -4,8 +4,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        @JvmField val ADD_NOTE_REQUEST = 1
+        @JvmField val EDIT_NOTE_REQUEST = 2//to not make getter,setter
+    }
 
     private var noteViewModel: NoteViewModel? = null
 
@@ -13,10 +21,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+
+        val adapter = NoteAdapter()
+        recyclerView.adapter = adapter
+
         noteViewModel = ViewModelProviders.of(this).get(NoteViewModel::class.java)
-        noteViewModel?.getAllNotesFromDB()?.observe(this, Observer {
-            it?.let { TODO("update recycler")
+        noteViewModel?.getAllNotesFromDB()?.observe(this, Observer { notes ->
+            notes?.let { adapter.submitList(it)}
+        })
+
+        noteViewModel?.insert(Note(0, "hola", "desc", 1))
+
+        adapter.setOnItemClickListener( object : NoteAdapter.OnItemClickListener {
+            override fun onItemClick(note: Note) {
+                TODO()
             }
         })
     }
+
+
 }
---
+++ app/src/main/java/com/example/mvvmkt/NoteAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.mvvmkt
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+
+class NoteAdapter : ListAdapter<Note, NoteAdapter.NoteHolder>(DiffCallback()) {
+
+    private var listener: OnItemClickListener? = null
+
+
+    class DiffCallback : DiffUtil.ItemCallback<Note>() {
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.description.equals(newItem.description) &&
+                    oldItem.title.equals(newItem.title) &&
+                    oldItem.priority == newItem.priority
+        }
+
+    }
+
+    class NoteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvTitle = itemView.findViewById<TextView>(R.id.text_view_title)
+        val tvDescription = itemView.findViewById<TextView>(R.id.text_view_description)
+        val tvPriority = itemView.findViewById<TextView>(R.id.text_view_priority)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
+        val itemView: View = LayoutInflater.from(parent.context)
+            .inflate(R.layout.note_item, parent, false)
+        return NoteHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        holder.itemView.setOnClickListener {
+            if (listener != null && position != RecyclerView.NO_POSITION) {
+                listener!!.onItemClick(getItem(position))
+            }
+        }
+
+        val note = getItem(position)
+        holder.tvTitle.text = note.title
+        holder.tvDescription.text = note.description
+        holder.tvPriority.text = note.priority.toString()
+
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(note: Note)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+}
